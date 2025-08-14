1ea79301f6a85fa47aeaad7f73b2539499baab21
TheoBourdel <bourdeltheo@gmail.com>
2025-08-14 13:08:40 +0200

    fix testimonial card responsive

--- components/ui/infinite-moving-cards.jsx
+++ components/ui/infinite-moving-cards.jsx
@@ -88,7 +88,7 @@ export const InfiniteMovingCards = ({
       <div
         ref={containerRef}
         className={cn(
-          "scroller relative z-20 max-w-7xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+          "scroller relative z-20 max-w-7xl w-full overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
           className
         )}
       >
@@ -101,9 +101,11 @@ export const InfiniteMovingCards = ({
         >
           {items.map((item) => (
             <li
-              className="relative w-[350px] max-w-full shrink-0 rounded-2xl border border-b-0 border-zinc-200 bg-[linear-gradient(180deg,#fafafa,#f5f5f5)] px-8 py-6 md:w-[450px] dark:border-zinc-700 dark:bg-[linear-gradient(180deg,#27272a,#18181b)]"
-              key={item.name}
-            >
+  className="relative w-[300px] sm:w-[320px] md:w-[450px] max-w-full shrink-0 rounded-2xl border border-b-0 border-zinc-200 
+             bg-[linear-gradient(180deg,#fafafa,#f5f5f5)] px-6 py-4 md:px-8 md:py-6
+             dark:border-zinc-700 dark:bg-[linear-gradient(180deg,#27272a,#18181b)]"
+  key={item.name}
+>
               <blockquote>
                 <span className="relative z-20 text-sm leading-[1.6] font-normal text-neutral-800 dark:text-gray-100">
                   {item.quote}
@@ -136,7 +138,7 @@ export const InfiniteMovingCards = ({
           onMouseUp={handleSliderEnd}
           onTouchStart={handleSliderStart}
           onTouchEnd={handleSliderEnd}
-          className="mt-4 w-full max-w-md custom-slider"
+          className="mt-4 px-4 w-full max-w-md custom-slider"
         />
       )}
     </div>
